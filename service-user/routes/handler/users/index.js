5697f122cb43b4aae0c6bbaa90314f208dc8b19e
Danny Suggi Saputra <dannysaputra3003@gmail.com>
2023-03-26 19:28:58 +0700

    make routes for get users and logout

--- service-user/routes/handler/users/index.js
+++ service-user/routes/handler/users/index.js
@@ -1,9 +1,15 @@
 const register = require('./register');
 const login = require('./login');
 const update = require('./update');
+const getUser = require('./getUser');
+const getListUsers = require('./getListUsers');
+const logout = require('./logout');
 
 module.exports = {
     register,
     login,
+    logout,
     update,
+    getUser,
+    getListUsers
 }
