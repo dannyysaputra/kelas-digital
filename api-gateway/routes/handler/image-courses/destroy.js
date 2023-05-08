b79309ccf69ba3f423b7d011f205ad2a1d8e0109
Danny Suggi Saputra <dannysaputra3003@gmail.com>
2023-05-08 21:00:19 +0700

    delete whitespace in condition error

--- api-gateway/routes/handler/image-courses/destroy.js
+++ api-gateway/routes/handler/image-courses/destroy.js
@@ -10,7 +10,7 @@ module.exports = async (req, res) => {
         const imageCourse = await axios.delete(`${URL_SERVICE_COURSE}/api/image-courses/${id}`);
         return res.json(imageCourse.data);
     } catch (error) {
-        if (error.code === 'ECONNREFUSED ') {
+        if (error.code === 'ECONNREFUSED') {
             return res.status(500).json({ status: 'error', message: 'service unavailable' });
         }
         const { status, data } = error.response;
