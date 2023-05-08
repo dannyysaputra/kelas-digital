9fa96f1992a709da0273d8396aaab0e6245ea377
Danny Suggi Saputra <dannysaputra3003@gmail.com>
2023-05-08 14:34:35 +0700

    make a handler for my courses

---
+++ api-gateway/routes/handler/my-courses/get.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+const {
+    URL_SERVICE_COURSE
+} = process.env;
+
+module.exports = async (req, res) => { 
+    try {
+        const userId = req.user.data.id;
+
+        const myCourses = await axios.get(`${URL_SERVICE_COURSE}/api/my-courses`, {
+            params: { user_id: userId }
+        });
+        return res.json(myCourses.data);
+    } catch (error) {
+        if (error.code === 'ECONNREFUSED ') {
+            return res.status(500).json({ status: 'error', message: 'service unavailable' });
+        }
+        const { status, data } = error.response;
+        return res.status(status).json(data);
+    }
+}; 
